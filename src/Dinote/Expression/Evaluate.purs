40632f5b9d333fb49173be245b9fbf2a61abc8d3
rightfold <rightfold@gmail.com>
2017-03-20 00:11:32 +0100

    Generalize vertex lookup in evaluate

--- src/Dinote/Expression/Evaluate.purs
+++ src/Dinote/Expression/Evaluate.purs
@@ -3,18 +3,17 @@ module Dinote.Expression.Evaluate
   ) where
 
 import Data.Foldable (sum)
-import Data.Map as Map
 import Dinote.Expression (Expression, ExpressionF(..))
 import Dinote.Prelude
 import Dinote.Vertex (VertexID(..))
 import Global (readFloat)
 import Matryoshka (cata)
 
-evaluate :: Map VertexID (Expression + String) -> Expression -> String
+evaluate :: (VertexID -> Maybe (Expression + String)) -> Expression -> String
 evaluate vertices = cata evaluate'
   where
   evaluate' (Var name) =
-    Map.lookup (VertexID name) vertices
+    vertices (VertexID name)
     # fromMaybe (Right "")
     # either (evaluate vertices) id
   evaluate' (Sum values) =
